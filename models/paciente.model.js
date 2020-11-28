1fc36373195ee8a2c818011857d4c35f4c010157
Christopher Delgado <christopher.delgado@gammapartners.com>
2020-11-28 15:11:06 -0700

    Pacientes v1

---
+++ models/paciente.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Paciente = mongoose.model(
+  "Paciente",
+  new mongoose.Schema({
+    nombre: String,
+    telefono: String,
+    foto: String,
+    codigo: String,
+    doctor:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+  })
+);
+
+module.exports = Paciente;
---
+++ routes/paciente.routes.js
@@ -0,0 +1,26 @@
+const { verifySignUp } = require("../middleware");
+const paciente = require("../controllers/paciente.controller");
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/api/paciente/signup",
+    paciente.signup
+  );
+
+  app.post("/api/paciente/signin", paciente.signin);
+
+  app.get("/api/pacientes", paciente.getPacientesDeDoctor);
+
+  app.get("/api/paciente/esp", paciente.getParcienteEspecifico);
+
+  app.put("/api/paciente/esp", paciente.updatePaciente);
+
+};
